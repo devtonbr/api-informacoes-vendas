33fd2461054d1fb3af11fedf4b940cbc14a70191
git <claytnmorais@gmail.com>
2023-11-14 20:00:50 -0300

    Projeto informações - 14/11/2023

---
+++ src/main/java/br/edu/infnet/api/informacoes/vendas/ApiInformacoesVendasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.api.informacoes.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInformacoesVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInformacoesVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/api/informacoes/vendas/ApiInformacoesVendasApplicationTests.java
@@ -0,0 +1,11 @@
+package br.edu.infnet.api.informacoes.vendas;
+
+import org.junit.jupiter.api.Test;
+
+class ApiInformacoesVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

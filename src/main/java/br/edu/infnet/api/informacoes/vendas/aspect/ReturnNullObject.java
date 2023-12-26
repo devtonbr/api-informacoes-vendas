bc54d1742f67b7322bfba59738361fe9cba9b968
claytonmorais <claytonmorais@gmail.com>
2023-12-26 12:36:44 -0300

    Update ReturnNullObject.java

--- src/main/java/br/edu/infnet/api/informacoes/vendas/aspect/ReturnNullObject.java
+++ src/main/java/br/edu/infnet/api/informacoes/vendas/aspect/ReturnNullObject.java
@@ -5,14 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Anotação para tratar o retorno do dao
- * 
- * OBS: esta anotação deve ser utilizada somente em classes DAO
- * 
- * @author Clayton Morais de Oliveira
- *
- */
+
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ReturnNullObject {
